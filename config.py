5a7b3267520376fa8050cfd0296921a40de6bd6e
xonicphonk-sys <xonicphonk@gmail.com>
2026-07-26 11:25:06 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# --- SuperBot Configuration ---
+
+BOT_TOKEN = "8965741278:AAGP596JO1bxSt3xDZJbMQVIuogc6MejJI8" 
+SUPPORT_CHANNEL = "https://t.me/+wqPogouCINoxOTM1"
+
+# ⚠️ গড-লেভেল এডমিনের জন্য আপনার টেলিগ্রাম একাউন্টের Numeric ID লাগবে।
+# (ID বের করতে টেলিগ্রামে @userinfobot এ গিয়ে /start দিন, একটি আইডি নাম্বার দেবে)
+ADMIN_ID = 6836865426  # এখানে আপনার আসল ID নাম্বার বসাবেন (যেমন: 543219876)
+
+ADMIN_USERNAME = "saddamadmin"
+ADMIN_PASSWORD = "saddamadmin1234"
